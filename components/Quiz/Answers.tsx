08a03da9474da37bdedfedafa9630601333b824b
Keitoto <keitasekihara@gmail.com>
2023-04-10 20:19:06 +0900

    ✨ Added target in Answers prop

--- components/Quiz/Answers.tsx
+++ components/Quiz/Answers.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from 'react';
-import { PlayerData } from '../../types';
+import { PlayerData, PlayerFullData } from '../../types';
 import { useStatsList } from '../../hooks/useStats';
 import Table from '../UI/Table';
 
 const Answers: FC<{
   answers: PlayerData[];
+  target: PlayerFullData;
 }> = ({ answers }) => {
   const { data: answeredPlayers, error, isLoading } = useStatsList(answers);
   if (isLoading || !answeredPlayers) return <div>Loading</div>;
